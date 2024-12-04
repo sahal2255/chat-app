e20f9aebc2b46a3bf3bfcc4fa87b3ddff5b1006d
sahal2255 <sahalvv9656@gmail.com>
2024-12-04 19:05:58 +0530

    ulr

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL:'http://localhost:5001/api',
+    baseURL:'https://chat-app-backend-lsx0.onrender.com',
     withCredentials:true 
 })
 
